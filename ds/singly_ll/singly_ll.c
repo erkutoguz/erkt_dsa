d5bbd331f71fa2dd60cf4d1935b6f56e40012661
Erkut Oguz <erkutoguz.eo@gmail.com>
2025-08-04 17:37:23 -0500

    data_structure: singly linked list structure implemented
    Signed-off-by: erkutoguz.eo@gmail.com

---
+++ ds/singly_ll/singly_ll.c
@@ -0,0 +1,240 @@
+#include "singly_ll.h"
+
+int main(int argc, char **argv)
+{
+	Node *n = create_node(10);
+	SLL *sll = create_sll(n);
+
+	int pos;
+	Node *n1 = create_node(20);
+	Node *n2 = create_node(30);
+	Node *n3 = create_node(40);
+	Node *n4 = create_node(50);
+	Node *n5 = create_node(60);
+	Node *n6;
+	Node *n7;
+	Node *n8;
+
+	insert_at_head(sll, n1);
+	insert_at_tail(sll, n2);
+	insert_at_head(sll, n3);
+	insert_at_tail(sll, n4);
+
+	printf("Size: %d\n", size(sll));
+	traverse(sll);
+
+	insert_at_pos(sll, n5, 2);
+
+	traverse(sll);
+
+	n6 = delete_at_head(sll);
+	printf("Deleted at head Node: %d\n", n6->val);
+
+	traverse(sll);
+
+	n7 = delete_at_tail(sll);
+	printf("Deleted at tail Node: %d\n", n7->val);
+
+	traverse(sll);
+
+	n8 = delete_at_pos(sll, 2);
+	printf("Deleted at pos Node: %d\n", n8->val);
+
+	traverse(sll);
+
+	pos = search(sll, 20);
+	printf("Search result: %d\n", pos);
+
+	update_at_pos(sll, 1, 90);
+
+	traverse(sll);
+
+	free_list(sll);
+
+	traverse(sll);
+
+	return 0;
+}
+
+SLL *create_sll(Node *node)
+{
+	SLL *sll = (SLL *)malloc(sizeof(SLL));
+	sll->head = node;
+	sll->tail = sll->head;
+	sll->size = 1;
+	return sll;
+}
+
+Node *create_node(int val)
+{
+	Node *node;
+	node = (Node *)malloc(sizeof(Node));
+	node->val = val;
+	return node;
+}
+
+void insert_at_head(SLL *sll, Node *node)
+{
+	if (is_empty(sll))
+		perror("list is empty");
+
+	node->next = sll->head;
+	sll->head = node;
+	sll->size++;
+}
+
+void insert_at_tail(SLL *sll, Node *node)
+{
+	if (is_empty(sll))
+		perror("list is empty");
+
+	node->next = NULL;
+	sll->tail->next = node;
+	sll->tail = node;
+	sll->size++;
+}
+
+void insert_at_pos(SLL *sll, Node *node, int pos)
+{
+	if (pos > size(sll))
+		perror("out of bounds");
+
+	int i;
+	i = 0;
+	Node *curr, *prev;
+	curr = sll->head;
+	prev = sll->head;
+	while (i++ < pos) {
+		prev = curr;
+		curr = curr->next;
+	}
+	prev->next = node;
+	node->next = curr;
+
+	prev = curr = NULL;
+	sll->size++;
+}
+
+Node *delete_at_head(SLL *sll)
+{
+	if (is_empty(sll))
+		perror("list is empty");
+
+	Node *deleted;
+	deleted = sll->head;
+	sll->head = sll->head->next;
+	deleted->next = NULL;
+	sll->size--;
+	return deleted;
+}
+
+Node *delete_at_tail(SLL *sll)
+{
+	if (is_empty(sll))
+		perror("list is empty");
+
+	Node *curr, *prev;
+
+	curr = prev = sll->head;
+
+	while (curr->next) {
+		prev = curr;
+		curr = curr->next;
+	}
+
+	prev->next = NULL;
+	sll->tail = prev;
+	prev = NULL;
+	sll->size--;
+	return curr;
+}
+
+Node *delete_at_pos(SLL *sll, int pos)
+{
+	if (pos > size(sll))
+		perror("out of bounds");
+
+	if (is_empty(sll))
+		perror("list is empty");
+
+	int i;
+	Node *curr, *prev;
+
+	i = 0;
+	curr = prev = sll->head;
+
+	while (i++ < pos) {
+		prev = curr;
+		curr = curr->next;
+	}
+
+	prev->next = curr->next;
+	prev = NULL;
+	sll->size--;
+	return curr;
+}
+
+int search(SLL *sll, int val)
+{
+	int pos;
+	Node *curr;
+
+	pos = 0;
+	curr = sll->head;
+
+	while (curr->next && curr->val != val) {
+		curr = curr->next;
+		++pos;
+	}
+
+	return curr->val == val ? pos : -1;
+}
+
+void update_at_pos(SLL *sll, int pos, int val)
+{
+	int i;
+	Node *curr;
+
+	i = 0;
+	curr = sll->head;
+
+	while (i++ < pos)
+		curr = curr->next;
+
+	curr->val = val;
+	curr = NULL;
+}
+
+int is_empty(SLL *sll)
+{
+	return sll->size == 0;
+}
+
+int size(SLL *sll)
+{
+	return sll->size;
+}
+
+void traverse(SLL *sll)
+{
+	if (is_empty(sll)) {
+		printf("List is empty\n");
+		return;
+	}
+
+	Node *curr;
+	curr = sll->head;
+	while (curr) {
+		printf("%d ", curr->val);
+		curr = curr->next;
+	}
+	printf("\n");
+}
+
+void free_list(SLL *sll)
+{
+	while (sll->head) {
+		sll->head = sll->head->next;
+		sll->size--;
+	}
+}
---
+++ ds/singly_ll/singly_ll.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_node Node;
+struct s_node {
+  int val;
+  Node *next;
+};
+
+struct s_sll{
+  int size;
+  Node *head;
+  Node *tail;
+};
+typedef struct s_sll SLL;
+
+SLL *create_sll(Node*);
+Node *create_node(int);
+
+/* insertion */
+void insert_at_head(SLL*,Node*);
+void insert_at_tail(SLL*,Node*);
+void insert_at_pos(SLL*,Node*,int);
+
+/* remove */
+Node *delete_at_head(SLL*);
+Node *delete_at_tail(SLL*);
+Node *delete_at_pos(SLL*,int);
+
+/* search */
+int search(SLL*,int);
+
+/* update */
+void update_at_pos(SLL*,int,int);
+
+/* other */
+void traverse(SLL*);
+int size(SLL*);
+int is_empty(SLL*);
+void free_list(SLL*);
+
+int main(int,char**);
